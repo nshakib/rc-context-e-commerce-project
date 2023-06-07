3a71c1a042dbd339c208631c84086314741a3c1f
nshakib <nazmus.shakib.cse@gmail.com>
2023-06-07 22:32:50 +0600

    api integration complate with strape

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,8 +4,26 @@ import { BsCartX } from "react-icons/bs";
 import CartItem from "./CartItem/CartItem";
 import { useContext } from "react";
 import { Context } from "../../utils/context";
+import { loadStripe } from "@stripe/stripe-js";
+import {makePaymentRequest} from "../../utils/api";
 const Cart = ({setShowCart}) => {
     const {cartItems, cartSubTotal} = useContext(Context)
+    const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+
+    const handlePayment = async () => {
+      try {
+        const stripe = await stripePromise;
+        const res = await makePaymentRequest.post("/api/orders", {
+          products: cartItems,
+        });
+
+        await stripe.redirectToCheckout({
+          sessionId: res.data.stripeSession.id,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
     return (
       <div className="cart-panel">
         <div className="opac-layer"></div>
@@ -30,11 +48,11 @@ const Cart = ({setShowCart}) => {
           <CartItem />
             <div className="cart-footer">
               <div className="subtotal">
-                <span className="text">Subtotal: </span>
-                <span className="text total">{cartSubTotal}</span>
+                <span className="text">Subtotal:</span>
+                <span className="text total">&#2547;{cartSubTotal}</span>
               </div>
               <div className="button">
-                <button className="checkout-cta">Checkout</button>
+                <button className="checkout-cta" onClick={handlePayment}>Checkout</button>
               </div>
             </div>           
           </>}
